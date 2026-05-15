1d4b70ede78274a679409fe4de1fbcc0933762bc
Eiman Males <maleseiman@gmail.com>
2026-05-14 21:56:44 -0500

    Creacion Plantilla Spring Boot

---
+++ src/main/java/com/ProgramacionV/Peluqueria/PeluqueriaApplication.java
@@ -0,0 +1,13 @@
+package com.ProgramacionV.Peluqueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeluqueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeluqueriaApplication.class, args);
+	}
+
+}
